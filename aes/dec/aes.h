d6b47d2a604762632587961da9f96bd63727ac43
feixiao <feixiao2020@sina.com>
2023-03-07 13:53:54 +0800

    update

--- aes/dec/aes.h
+++ aes/dec/aes.h
@@ -1,16 +1,7 @@
-#ifndef _AES_H
-#define _AES_H
-
-
 #include <iostream>
-#include <string>
-
-
-int aes_cbc_decrypt(const std::string& ciphertext, const std::string& key,
-     const std::string& iv, std::string& plaintext);
 
 int aes_cbc_encrypt(const std::string& plaintext, const std::string& key, 
      const std::string& iv, std::string& ciphertext);
 
-
-#endif
+int aes_cbc_decrypt(const std::string& ciphertext, const std::string& key,
+     const std::string& iv, std::string& plaintext);
